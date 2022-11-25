c9c6561e84b83890ae0e9fca17f8ede3c3981d12
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-11-24 23:21:58 -0300

    Resolução-exercicio1-strings

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+/*
+RESOLUÇÃO: variavel com prompt para armazenar o que vai ser digitado dentro da mesma. Console.log + place holder crazeado para imprimir os dados digitados.
+*/
+
+alert("Salve professor, tudo bem?")
+let nome = prompt("Qual é seu nome?")
+let comida1 = prompt("Qual sua comida favorita?")
+let comida2 = prompt("Qual é sua segunda comida favorita?")
+let comida3 = prompt("Qual sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas do ${nome} \n ${comida1} \n ${comida2} \n ${comida3}.`)
+
+
+
+
